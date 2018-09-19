27ef267bb43543b8be0abc62f397120cca05d063
Eric Chrobak <eric@ericchrobak.com>
2018-09-19 20:21:01 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,22 +8,13 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     char = self.characters
-    binding.pry
-    if char.length > 1
+    if char.length == 1
+      name = char.name
+    elsif char.length > 1
       binding.pry
       char.each do  |a|
         "#{a[name]} - "
-        binding.pry
       end
-    else
-      binding.pry
-      name = char.name
     end
   end
-end
-
-if char.length > 1
-  "Greater than 1"
-else
-  "Equal to or less than 1"
 end
